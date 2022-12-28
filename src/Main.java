20915e731be1a6a53e590a5a50280cbdcc76f324
kirsan07 <terkirsun@gmail.com>
2022-12-28 19:20:13 +0900

    Hello world

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+int a = scanner.nextInt();
+int b = scanner.nextInt();
+int max = 0;
+if (a>b) {
+    max = a;
+}
+else {
+    max = b;
+}
+System.out.println(max);
+git config --global user.email ""
+    }
+}
